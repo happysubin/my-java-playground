1e1deb3c0ae01e1f46abedaa8ae49854efebcbdd
happysubin <76802855+happysubin@users.noreply.github.com>
2021-08-11 00:01:05 +0900

    코드업 1001 - 1015

--- Hello World/src/Hello.java
+++ Hello World/src/Hello.java
@@ -1,4 +1,5 @@
-
+//코드업 100제 1001번     이클립스는 클래스와 파일이름이 같아야한다!
+/*
 public class Hello {
 
 	public static void main(String[] args) {
@@ -6,4 +7,143 @@ public class Hello {
 
 	}
 
+}*/
+
+//1002
+/*public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
 }
+*/
+
+//1003
+/*public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello \nWorld");
+    }
+}*/
+
+//1004
+/*
+ public class Hello{
+    public static void main(String[] args){
+        System.out.println("\'Hello\'");  \' 으로 쉼표를 쓴다
+    }
+} 
+ */
+ //1005
+/* public class Hello{
+    public static void main(String[] args){
+        System.out.println("\"Hello World\"");  
+    }
+} 
+*/
+//1006
+/*
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\"!@#$%^&*()\"");
+    }
+}
+*/
+//1007
+/*
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}
+*/
+//1008
+/*public class Hello{
+	public static void main(String[] args) {
+		System.out.println("\u250C\u252C\u2510\n");
+	}
+}*/
+
+//1010
+/*
+ import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num;
+        num=sc.nextInt();
+        System.out.println(num);
+    }
+}
+ */
+
+//1011
+/*
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.next();  sc.nextLine는 한줄을 다 받는다
+        System.out.println(s);
+    }
+}
+//1012
+ * 
+ import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float n;
+        n=sc.nextFloat();
+        System.out.printf("%f",n);  //printf 로 부동소수점 표현가능. c언어와 매우 비슷해보인다.
+    }
+    
+}
+ */
+
+//1013
+
+/* 
+ * import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.printf("%d %d",a,b);
+    }
+}
+ */
+//1014
+
+/*
+ import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String a,b;
+        a=sc.next();
+        b=sc.next();
+        System.out.println(b+" "+a);
+    }
+}
+
+//1015
+ /*
+  import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a;
+        a=sc.nextDouble();
+        System.out.printf("%.2f",a);
+        
+    }
+}
+  */
+ 
