cc9cdf324f24e6430d11cd138e794cfed1f71549
happysubin <76802855+happysubin@users.noreply.github.com>
2021-08-18 00:14:07 +0900

    백준 문자열 단계 첫 4문제

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -1,187 +1,81 @@
-//1085
-/*
- * import java.util.Scanner;
+//백준 문자열 문제들
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String[] str=sc.nextLine().split(" ");
-		long a=Integer.valueOf(str[0]);
-		long b=Integer.valueOf(str[1]);
-		long c=Integer.valueOf(str[2]);
-		long d=Integer.valueOf(str[3]);
-		double result=((((a*b*c*d)/8)/Math.pow(2, 10))/Math.pow(2, 10));
-		System.out.printf("%.1f MB",result);
-	}
-}
-*/
-
-//1086
+//아스키 코드
 /*
 import java.util.Scanner;
-
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String[] str=sc.nextLine().split(" ");
-		long a=Integer.valueOf(str[0]);
-		long b=Integer.valueOf(str[1]);
-		long c=Integer.valueOf(str[2]);
-		double result=((((a*b*c)/8)/Math.pow(2, 10))/Math.pow(2, 10));
-		System.out.printf("%.2f MB",result);
-	}
-}
-*/
-
-//1087
-
-/*
-import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt(); 
-        int sum=0;
-        for(int i=1;i<=num;i++){
-            sum=sum+i;
-            if(sum>=num){
-                System.out.printf("%d", sum);
-                break;
-            }
-        }
+        char a=sc.next().charAt(0);
+        System.out.print((int)a);
     }
 }
 */
 
-//1088
-/*
+/*숫자의 합
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
-        for(int i=0;i<=num;i++){
-            if(i%3!=0) System.out.printf("%d ",i);
+        String[] str=sc.next().split("");
+        int sum=0;
+        for(int i=0;i<num;i++){
+            sum=sum+Integer.parseInt(str[i]);
         }
+        System.out.print(sum);
     }
 }
 */
+// 자바는 int 배열을 선언하면 초기값은 0이고 string 배열의 초기값은 null이다.
 
-//1089
+//백준 문자열반복 2675
 /*
 import java.util.Scanner;
-
 public class Main{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String[] str=sc.nextLine().split(" ");
-        int a=Integer.valueOf(str[0]);
-        int b=Integer.valueOf(str[1]);
-        int c=Integer.valueOf(str[2]);
-        System.out.printf("%d", a+b*(c-1));
-    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int cnt=0;
+		
+		for(int i=0;i<num;i++) {
+			cnt=sc.nextInt();
+			String[] arr=sc.next().split("");
+			
+			for(int j=0;j<arr.length;j++) {
+				for(int z=0;z<cnt;z++) {
+					System.out.print(arr[j]);
+				}
+			}
+			System.out.println();
+		}
+		
+	}
 }
 */
 
-//1090
-/*
 import java.util.Scanner;
-
+//소문자만 처음에 사용된 인덱스를 표시
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        String name=sc.next(); //이름을 받는다
+        int[] cnt=new int[26]; // 알파벳을 위한 배열
         
-        String[] str=sc.nextLine().split(" ");
-        int a=Integer.valueOf(str[0]);
-        int b=Integer.valueOf(str[1]);
-        int c=Integer.valueOf(str[2]);
-        System.out.printf("%.0f", a*Math.pow(b,c-1));
-    }
-}
-*/
-
-//1091
-/*
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String[] str=sc.nextLine().split(" ");
-        long a=Integer.valueOf(str[0]);
-        long b=Integer.valueOf(str[1]);
-        long c=Integer.valueOf(str[2]);
-        long d=Integer.valueOf(str[3]);
-        for(int i=1;i<d;i++ ){
-            a=(a*b)+c;
+        for(int i=0;i<cnt.length;i++){
+            cnt[i]=-1; //사용하지 않은 알파벳은 -1을 출력하므로 -1로 초기화
         }
-        System.out.printf("%d", a);
-    }
-} 시작한수가 1번째 수열이니 예를들어 13번 반복이면 12번 해줘야한다.
-*/
-
-//1092
-/*
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String[] str=sc.nextLine().split(" ");
-        int a=Integer.valueOf(str[0]);
-        int b=Integer.valueOf(str[1]);
-        int c=Integer.valueOf(str[2]);
-        int day=1;
-        while((day%a!=0)||(day%b!=0)||(day%c!=0))day++; 최소공배수때 whle문이 멈춘다고 생각.
-        System.out.println(day);
-    }
-}
-*/
-
-//1093
-/*
-
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int[] arr=new int[23];
-        int num=0;
+        for(int i=0;i<name.length();i++){
+            char content=name.charAt(i); //이름 문자열의 문자들을 다꺼낸다
+            if(cnt[content-'a']==-1) { //초기화된 값이 아니면 값을 저장하지 않는다.
+            	cnt[content-'a']=i;
+            }// a를 기준으로 소문자만 사용되므로 a를 빼서 인덱스로 사용 b의 인덱스는 1 c는 2 이런 식 
+            //사용된 자리는 i를 통해 나타내므로 i를 배열의 인덱스 요소로 저장.
+        }
+        for(int i=0;i<cnt.length;i++){
+            System.out.print(cnt[i]+" ");
+        }
         
-        // 이 부분이 중요했다.
-        for(int i=0;i<a;i++){
-            num=sc.nextInt();
-            arr[num-1]+=1;
-        }      
-        //
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
     }
 }
-*/
-
-//1094
-/*
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int num=sc.nextInt();
-    int [] arr=new int[num];
-    for(int i=0; i<arr.length;i++){
-        arr[i]=sc.nextInt();
-    }
-    for(int i=arr.length-1; i>=0;i--){
-        System.out.printf("%d ",arr[i]);
-    }
-    
-    }
-}
-*/
-
-//1095
