2c91f2ae6eea5148844bd298b7858ab1acf7df0e
happysubin <76802855+happysubin@users.noreply.github.com>
2021-09-11 13:53:48 +0900

    백준 소수

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -1100,6 +1100,7 @@ public class Main{
 }
 */
 
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -1129,5 +1130,45 @@ public class Main{
 		}
 		System.out.println(cnt);
 		br.close();
+	}
+}
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int sum=0;
+		int low=Integer.parseInt(br.readLine()); 
+		int high=Integer.parseInt(br.readLine()); 
+		int min=high;
+		for(int i=low; i<=high;i++) { //받은 수의 전체 범위를 돌린다.
+			int cnt=0;
+			
+			if(i==1)continue;
+			
+			for(int j=2;j<=i;j++) {
+				if(i%j==0) {
+					cnt++;
+				}
+			}
+			
+			if(cnt==1) {
+				sum+=i;
+				if(i<min)min=i;
+			}
+		}	
+		
+		
+		if(sum !=0) {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+		else {
+			System.out.println(-1);
+		}
 	}
 }
