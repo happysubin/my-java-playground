edb204b2cfdaa9b3a7f8585e27793ce1b8c07f12
happysubin <76802855+happysubin@users.noreply.github.com>
2021-09-30 15:55:50 +0900

    백준 그리디 알고리즘 1439

--- Hello World/src/greed.java
+++ Hello World/src/greed.java
@@ -1,4 +1,5 @@
 
+/*
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -21,9 +22,44 @@ public class greed {
 			sum+=Integer.parseInt(arr1[i])*Integer.parseInt(arr2[num-i-1]);
 		}
 		
-		for(int i=0;i<num;i++)System.out.print(arr1[i]);
-		for(int i=0;i<num;i++)System.out.print(arr2[num-i-1]);
 		System.out.println(sum);
 		
+	}
+}
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class greed{
+	public static void main(String[] args)throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String str=br.readLine();
+		int cnt=0;
+		
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(0)=='1') { //1로 시작할때
+				if(i<str.length()-1) { //마지막 숫자 전까지
+					if(str.charAt(i)==str.charAt(i+1)) {
+						continue;
+					}else if(str.charAt(i)=='1'&&str.charAt(i+1)=='0'){
+						cnt++;
+					}
+				}
+			}
+			else {//0으로 시작할때
+				if(i<str.length()-1) { //마지막 숫자 전까지
+					if(str.charAt(i)==str.charAt(i+1)) { //1과 같으면 진행
+						continue;
+					}else if(str.charAt(i)=='0'&str.charAt(i+1)=='1'){
+						cnt++;
+					}
+				}
+				
+			}
+		}
+		System.out.println(cnt);
 	}
 }
