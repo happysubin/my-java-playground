ed09cff5444bfb87623f6c604db5cea6a4bd6b1a
happysubin <76802855+happysubin@users.noreply.github.com>
2021-10-05 17:20:39 +0900

    자바 lecture

--- Hello World/src/javaClass.java
+++ Hello World/src/javaClass.java
@@ -1,4 +1,6 @@
 
+/*
+
 import java.util.Scanner;
 import java.lang.ArithmeticException;
 import java.util.InputMismatchException;
@@ -53,4 +55,51 @@ public class javaClass{
 		
 	}
 }
+*/
 //문제점 분자로 0이 들어오면 안된다.
+
+import java.util.Scanner;
+
+class Book{
+	String title,author,year;
+	
+	public Book(String title, String author) {
+		this(title,author,"발간년도미상");
+	}
+	public Book(String title, String author,String year) {
+		this.title=title;
+		this.author=author;
+		this.year=year;
+	}
+}
+	
+
+public class javaClass{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		Book[] book=new Book[2];
+		for(int i=0;i<book.length;i++) {
+			System.out.println("제목>>");
+			String title=sc.nextLine();
+			
+			System.out.println("저자>>");
+			String author=sc.nextLine();
+			
+			System.out.println("발간년도>>");
+			String year=sc.nextLine();
+			
+			if(year=="") {
+				book[i]=new Book(title,author);
+			}
+			else {
+				book[i]=new Book(title,author,year);
+			}
+			
+		}
+		for(int i=0;i<book.length;i++) {
+			System.out.print("(" + book[i].title + ", " + book[i].author+ ", " + book[i].year + ")");
+		}
+		sc.close();
+		
+	}
+}
