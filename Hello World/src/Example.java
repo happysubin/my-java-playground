19dd9d9356340f20bd03e01a4e7572d61d9cbfc6
happysubin <76802855+happysubin@users.noreply.github.com>
2021-10-11 22:17:29 +0900

    자바 강의 실습1 복습

--- Hello World/src/Example.java
+++ Hello World/src/Example.java
@@ -1,25 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.io.IOException;
+/*
+ * a>>b
+ * a의 각 비트를 오른쪽으로 b번 시프트한다. 최상위 비트의 빈자리는 시프트 전의 최상위 비트로 다시 채운다.
+ * 산술적 오른쪽 시프트라고 한다.
+ * 
+ * a>>>b
+ * a의 각 비트를 오른쪽으로 b번 시프트하다. 그리고 최상위 비트의 빈자리는 0으로 채운다.ㄴ
+ * 논리적 오른쪽 시프트라고한다.
+ * 
+ * a<<b
+ * a의 각 비트를 왼쪽으로 b번 시프트한다. 그리고 최하위 비트의 빈자리는 0으로 채운다.
+ * 산술적 왼쪽 시프트라고 한다.
+ * 
+ * 실습 1 복습
+ */
+
+import java.util.Scanner;
 
 public class Example{
-	public static void main(String[] args)throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int num=Integer.parseInt(br.readLine());
-		String[] str=br.readLine().split(" ");
-		int[] arr=new int[num];
-		int score=0;
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		System.out.println(Integer.toBinaryString(num));
 		
-		for(int i=0;i<num;i++)arr[i]=Integer.parseInt(str[i]);
-		
-		Arrays.sort(arr);
-		
-		for(int i=num-1;i>0;i--) {
-			score+=arr[i]*arr[i-1];
-			arr[i-1]=arr[i]+arr[i-1];
-		}
-		System.out.println(score);
-		
+		int num2=sc.nextInt();
+		num=num>>>--num2;
+		System.out.println(num2&1);
 	}
 }
+
