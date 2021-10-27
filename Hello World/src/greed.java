f7ba8a266792c0628da1f23b6ab65e8756c4beb1
subinahn-gym <sb.ahn@gymmon.work>
2021-10-28 00:16:38 +0900

    백준 그리드 알고리즘 2217

--- Hello World/src/greed.java
+++ Hello World/src/greed.java
@@ -270,7 +270,7 @@ public class greed{
 	}
 }
 */
-
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -289,5 +289,30 @@ public class greed {
 			else {num-=1;cnt++;}
 		}
 		System.out.println(cnt);
+	}
+}
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class greed{
+	public static void main(String[] args)throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int num=Integer.parseInt(br.readLine());
+		int[] arr=new int[num];
+		int max=0;
+		for(int i=0;i<num;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		int j=num;
+		for(int i=0;i<num;i++) {
+			int sum=arr[i]*j--;
+			if(max<sum)max=sum;
+		}
+		System.out.println(max);
 	}
 }
