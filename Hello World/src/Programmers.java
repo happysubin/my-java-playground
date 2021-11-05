84378ab220a16eb7cace0e0ca676d54a6542a4f1
subinahn-gym <sb.ahn@gymmon.work>
2021-11-05 21:33:27 +0900

    프로그래머스 음양더하기

--- Hello World/src/Programmers.java
+++ Hello World/src/Programmers.java
@@ -35,6 +35,7 @@ public class Programmers {
 	    }
 	}
 	*/
+	/*
 	class Solution {
 	    public int solution(String s) {
 	        int answer = 0;
@@ -53,6 +54,20 @@ public class Programmers {
 	        return answer;
 	    }
 	}
-	
+	*/
+	class Solution {
+	    public int solution(int[] absolutes, boolean[] signs) {
+	        int sum=0;
+	        for(int i=0; i<absolutes.length;i++){
+	            if(signs[i]==false)absolutes[i]*=-1;
+	        }
+	        for(int i=0; i<absolutes.length;i++){
+	            sum+=absolutes[i];
+	        }
+	        
+	        int answer = sum;
+	        return answer;
+	    }
+	}
 
 }
