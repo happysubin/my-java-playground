18a3b7b2c6f3382aa6553da592d0d1a2a83e710d
happysubin <asb0711@gmail.com>
2021-12-15 00:28:50 +0900

    코딩테스트 자바 강의 첫 문자열 문제 문자 찾기 강사님 풀이

--- Hello World/src/FirstChapter.java
+++ Hello World/src/FirstChapter.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class FirstChapter {
@@ -19,5 +18,29 @@ public class FirstChapter {
         String check=sc.next();
         System.out.println(Solution(str,check));
         sc.close();
+    }
+}
+
+//1번 문자 찾기 해설
+
+class TeacherSolution{
+
+    public int solution(String str,char c){
+        int answer=0;
+        str=str.toUpperCase();
+        c=Character.toUpperCase(c);
+        for(char x: str.toCharArray()){//새로운 문자 배열이 하나 생성. 인덱스 0부터 시작
+            if(x==c)answer++;
+        } 
+            
+        return answer;
+    }
+    public static void main(String[] args){
+        TeacherSolution T=new TeacherSolution();
+        Scanner kb=new Scanner(System.in);
+        String str=kb.next();
+        char c=kb.next().charAt(0);
+        System.out.println(T.solution(str,c));
+        kb.close();
     }
 }
