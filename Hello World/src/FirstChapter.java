d57ab0b88d5c2580173510e4fa750f32958fd150
happysubin <asb0711@gmail.com>
2021-12-17 00:58:52 +0900

    코딩테스트 자바 강의 문자열 회문 문자열 강사님 풀이

--- Hello World/src/FirstChapter.java
+++ Hello World/src/FirstChapter.java
@@ -324,6 +324,7 @@ public class FirstChapter {
     }
 }
 */
+/*
 import java.io.*;
 
 public class FirstChapter{
@@ -360,4 +361,45 @@ public class FirstChapter{
         String str=br.readLine();
         System.out.println(T.solution(str));
     }
+}
+*/
+
+
+import java.util.*;
+class Main {	
+	public String solution(String str){
+		String answer="YES";
+		str=str.toUpperCase();
+		int len=str.length();
+		for(int i=0; i<len/2; i++){
+			if(str.charAt(i)!=str.charAt(len-i-1)) answer="NO";
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
+}
+
+
+
+import java.util.*;
+class Main {	
+	public String solution(String str){
+		String answer="NO";
+		String tmp=new StringBuilder(str).reverse().toString();
+		if(str.equalsIgnoreCase(tmp)) answer="YES";
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
 }
