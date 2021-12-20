c2ed646c03af771a99ffb27c36e77d74d484a283
happysubin <asb0711@gmail.com>
2021-12-20 13:29:22 +0900

    코딩테스트 자바 강의 문자열 암호 강사님 풀이

--- Hello World/src/FirstChapter.java
+++ Hello World/src/FirstChapter.java
@@ -477,6 +477,8 @@ class Main {
 }
 */
 
+/*
+
 import java.io.*;
 
 public class FirstChapter{
@@ -501,4 +503,28 @@ public class FirstChapter{
         String str=br.readLine();
         T.solution(str,num);
     }
+}
+
+*/
+
+import java.util.*;
+class Main {	
+	public String solution(int n, String s){
+		String answer="";
+		for(int i=0; i<n; i++){
+			String tmp=s.substring(0, 7).replace('#', '1').replace('*', '0'); //0부터 6까지 7개들을 잘라서 옮긴다.
+			int num=Integer.parseInt(tmp, 2); 
+			answer+=(char)num;//문자열에 문자를 더함
+			s=s.substring(7); //앞의 7문자를 계속 잘라낸다. 28개가 21개 21개가 14개 14개가 7개 7개가 0개로 문자열이 작아진다.
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		String str=kb.next();
+		System.out.println(T.solution(n, str));
+	}
 }
