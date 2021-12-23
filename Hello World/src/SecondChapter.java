0e8a443c937f237a9ec394298b08587ba87942de
happysubin <asb0711@gmail.com>
2021-12-23 13:47:54 +0900

    코테 강의 배열 뒤집은 소수 강사님 풀이

--- Hello World/src/SecondChapter.java
+++ Hello World/src/SecondChapter.java
@@ -267,6 +267,7 @@ public class SecondChapter{
 */
 
 
+/*
 import java.io.*;
 
 
@@ -299,5 +300,44 @@ public class SecondChapter{
 		String s=br.readLine();
 		System.out.print(T.solution(num,s));
 
+	}
+}
+*/
+
+import java.util.*;
+class Main {	
+	public boolean isPrime(int num){
+		if(num==1) return false;
+		for(int i=2; i<num; i++){
+			if(num%i==0) return false; //자기자신이 나오기전에 0으로 나누어지면 소수가 아니다.
+		}
+		return true;
+	}
+
+	public ArrayList<Integer> solution(int n, int[] arr){
+		ArrayList<Integer> answer = new ArrayList<>();
+		for(int i=0; i<n; i++){
+			int tmp=arr[i];
+			int res=0;
+			while(tmp>0){
+				int t=tmp%10;
+				res=res*10+t;
+				tmp=tmp/10;
+			}
+			if(isPrime(res)) answer.add(res);
+		}
+		return answer;
+	}
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++){
+			arr[i]=kb.nextInt();
+		}
+		for(int x : T.solution(n, arr)){
+			System.out.print(x+" ");
+		}
 	}
 }
