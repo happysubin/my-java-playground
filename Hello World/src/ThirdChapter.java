262094393122e3b2cef1470dbe421320b2ba9293
happysubin <asb0711@gmail.com>
2021-12-28 21:55:22 +0900

    코테 강의 두 배열 합치기 강사님 풀이

--- Hello World/src/ThirdChapter.java
+++ Hello World/src/ThirdChapter.java
@@ -29,6 +29,8 @@ public class ThirdChapter {
 
 */
 
+/*
+
 import java.util.Scanner;
 
 public class ThirdChapter{
@@ -69,3 +71,37 @@ public class ThirdChapter{
 }
 
 //이런 쉬운 정렬 문제가 만약에 나온다면 투 포인터를 쓰는 지 확인하기위함이므로 이렇게 풀자!!!
+
+
+*/
+
+import java.util.*;
+class Main {	
+	public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
+		ArrayList<Integer> answer = new ArrayList<>();
+		int p1=0, p2=0;
+		while(p1<n && p2<m){
+			if(a[p1]<b[p2]) answer.add(a[p1++]);
+			else answer.add(b[p2++]);
+		}
+		while(p1<n) answer.add(a[p1++]);
+		while(p2<m) answer.add(b[p2++]);
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[] a=new int[n];
+		for(int i=0; i<n; i++){
+			a[i]=kb.nextInt();
+		}
+		int m=kb.nextInt();
+		int[] b=new int[m];
+		for(int i=0; i<m; i++){
+			b[i]=kb.nextInt();
+		}
+		for(int x : T.solution(n, m, a, b)) System.out.print(x+" ");
+	}
+}
