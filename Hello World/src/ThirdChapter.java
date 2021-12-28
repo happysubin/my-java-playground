8b6fa471807b2bebf45d2fdcc8c83666a4c0c832
happysubin <asb0711@gmail.com>
2021-12-29 00:23:29 +0900

    코테 강의 공통 원소 구하기 (투 포인터 사용)

--- Hello World/src/ThirdChapter.java
+++ Hello World/src/ThirdChapter.java
@@ -111,6 +111,8 @@ class Main {
 
 */
 
+/*
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -165,6 +167,56 @@ public class ThirdChapter {
         }        
         
         for(int x: T.solution(num1, num2, arr1, arr2)){
+            System.out.print(x+" ");
+        }
+    }
+}
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ThirdChapter{
+    public ArrayList<Integer> solution(int num1, int num2, int[] arr1, int[] arr2){
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int p1=0,p2=0;
+        while(p1<num1 & p2<num2){
+            if(arr1[p1]==arr2[p2]){
+                answer.add(arr1[p1++]);
+                p2++;
+            }
+            else if(arr1[p1]<arr2[p2])p1++;
+            else p2++;
+        }
+        return answer;
+    }
+    public static void main(String[] args)throws IOException{
+        ThirdChapter T=new ThirdChapter();
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int num1=Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int[] arr1=new int[num1];
+        int i=0;
+        while(st.hasMoreTokens()){
+            arr1[i++]=Integer.parseInt(st.nextToken());
+        }
+
+        int num2=Integer.parseInt(br.readLine());
+        st=new StringTokenizer(br.readLine());
+        int[] arr2=new int[num2];
+        i=0;
+        while(st.hasMoreTokens()){
+            arr2[i++]=Integer.parseInt(st.nextToken());
+        }
+
+        for(int x:T.solution(num1, num2, arr1, arr2)){
             System.out.print(x+" ");
         }
     }
