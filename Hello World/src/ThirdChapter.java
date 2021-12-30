0771fb3125cee0620a9f592e4c140fd0a374b473
happysubin <asb0711@gmail.com>
2021-12-30 18:00:26 +0900

    코테 강의 연속 부분 수열

--- Hello World/src/ThirdChapter.java
+++ Hello World/src/ThirdChapter.java
@@ -269,6 +269,8 @@ public class ThirdChapter{
 
 */
 
+/*
+
 import java.util.Scanner;
 
 public class ThirdChapter{
@@ -297,3 +299,55 @@ public class ThirdChapter{
 		System.out.print(T.solution(n, k, arr));
     }
 }
+
+*/
+
+/*
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ThirdChapter{
+
+    public int solution (int total, int goal, int[] arr){
+   
+        int cnt=0;
+
+        // 1 2 1 3 1 1 1 2  
+        //순서를 적어보자면 차례로 더 하다가 값이 맞으면 멈춘다. 왜냐면 어차피 숫자가 1보다 커 더해지므로 더 이상의 경우는 없기 때문.
+        //그러면 1에서 모든 수를 구하면 2 다음은 1 다음은 3 이렇게 구하는 생각이였다.
+        for(int i=0; i<total;i++){  
+            int sum=0;
+            for(int j=i;j<total;j++){
+                sum+=arr[j];
+                if(goal==sum){ 
+                    cnt++;
+                    break;
+                }
+
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args)throws IOException{
+        ThirdChapter T=new ThirdChapter();
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String[] s=br.readLine().split(" ");
+        int total=Integer.parseInt(s[0]);
+        int goal=Integer.parseInt(s[1]);
+        int[] arr=new int[total];
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int i=0;
+        while(st.hasMoreTokens()){
+            arr[i++]=Integer.parseInt(st.nextToken());
+        }
+
+        System.out.print(T.solution(total, goal, arr));
+    }
+}
+
+*/
+
+//시간 초과로 삼중 포문은 실패
+//이것도 시간 초과.. ㅇㄴ
