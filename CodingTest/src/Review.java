461bf085da8a723c5836fd8c0688956c2a746870
happysubin <asb0711@gmail.com>
2022-02-02 21:45:41 +0900

    재시작

---
+++ CodingTest/src/Review.java
@@ -0,0 +1,8 @@
+
+import java.io.*;
+public class Review {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("br = " + br.readLine());
+    }
+}
