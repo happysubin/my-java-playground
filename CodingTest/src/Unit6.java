6d24027557d23652aaa0f2fa9b64a5d1d809d5cf
happysubin <asb0711@gmail.com>
2022-02-14 15:00:48 +0900

    코테 자바 6 - 7, 6 - 8

--- CodingTest/src/Unit6.java
+++ CodingTest/src/Unit6.java
@@ -183,7 +183,7 @@ public class Unit6 {
 6- 5 중복 문자 체크
  */
 
-
+/*
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -216,3 +216,86 @@ public class Unit6 {
         }
     }
 }
+
+6 - 6 장난꾸러기
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+class Point implements Comparable<Point>{
+    public int x,y;
+    Point(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if(this.x==o.x)return this.y-o.y;
+        //리턴 값이 음수 또는 0이면 자리가 유지됨. 양수면 자리가 교체됨. 우리는 올림차순
+            //즉 this 가 더 크면 양수가 나오는데, 올림차순인데 이게 더 크면 말이 안되므로 뒤집는다.
+                //그러나 정상적인 상태면 this 가 아래에 있으므로 값이 더 작으므로 파라미터가 값이 더커서 음수가 리턴된다.
+        //우리는 올림차순이기 때문에 음수가 리턴된다. 즉 자리가 유지.
+        else return this.x-o.x;
+    }
+}
+
+
+public class Unit6{
+
+    public static void main(String[] args)  {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        ArrayList<Point> arr=new ArrayList<>();
+        for(int i=0;i<num;i++){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            arr.add(new Point(x,y));
+        }
+        Collections.sort(arr); //sort메소드를 이용하면서 우리가 오버라이딩한 compareTo가 적용된다. 즉 정렬의 기준이 적용된다.
+        for (Point o : arr) {
+            System.out.println(o.x+" "+o.y);
+        }
+
+
+    }
+}
+
+//6 - 7 좌표 정렬
+
+
+/*
+import java.io.*;
+import java.util.Arrays;
+
+public class Unit6 {
+    public static int solution(int len,int aim, int[] arr) {
+        Arrays.sort(arr);
+        int answer=0;
+        for(int i=0;i<len;i++){
+            if(arr[i]==aim)answer=i+1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr=br.readLine().split(" ");
+        int len=Integer.parseInt(arr[0]);
+        int aim=Integer.parseInt(arr[1]);
+        String[] str=br.readLine().split(" ");
+        int[] list=new int[len];
+        int j=0;
+
+        for(String x: str){
+            list[j++]=Integer.parseInt(x);
+        }
+        System.out.println( solution(len,aim,list)+" ");
+    }
+}
+
+6 - 8 이분검색
+ */
