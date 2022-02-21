54cf99174c246d921d44c0c5b542f6d2dc28d662
happysubin <asb0711@gmail.com>
2022-02-21 20:57:05 +0900

    코테 자바 마구간 정하기

--- CodingTest/src/Unit6.java
+++ CodingTest/src/Unit6.java
@@ -320,6 +320,7 @@ public class Unit6 {
 6 - 8 이분검색
  */
 
+
 /*
 
 import java.io.*;
@@ -369,5 +370,59 @@ public class Unit6 {
         System.out.println(solution(len,sec,list)+" ");
     }
 }
-*/
 
+
+
+ */
+
+
+
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Unit6 {
+    public static int count(int[] arr, int dist){
+        int cnt=1;//최소 말 한마리를 배치
+        int ep=arr[0];//제일 작은 왼쪽 좌표에 배치
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]-ep>=dist){
+                cnt++;//말의 개수를 증가
+                ep=arr[i];//다음에 비교할 좌표에 넣는다
+            }
+        }
+        return cnt;
+    }
+    public static int solution(int len,int num, int[] arr) {
+        int lt=1; //말 사이의 최소 거리는 1
+        int rt=arr[len-1]-arr[0]; //최댓값은 맨 처음 좌표와 맨 마지막 좌표의 차이
+        int answer=0;//거리
+
+        Arrays.sort(arr); //좌표를 정렬
+
+        while(lt<=rt){
+            int mid=(lt+rt)/2; //mid가 말 사이의 거리
+            if(count(arr,mid)>=num){//말이 좌표에 얼마나 들어갔는지 체크
+                answer=mid;
+                lt=mid+1;
+            }
+            else rt=mid-1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr=br.readLine().split(" ");
+        int len=Integer.parseInt(arr[0]);
+        int num=Integer.parseInt(arr[1]);
+        String[] str=br.readLine().split(" ");
+        int[] list=new int[len];
+        int j=0;
+        for(String x: str){
+            list[j++]=Integer.parseInt(x);
+        }
+
+        System.out.println(solution(len,num,list));
+    }
+}
