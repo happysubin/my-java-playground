29cda07855bd5a585b05395496185839134902e2
happysubin <asb0711@gmail.com>
2022-05-14 15:02:06 +0900

    init

---
+++ src/main/java/happysubin/javapractice/stream/problem/Transaction.java
@@ -0,0 +1,34 @@
+package stream.problem;
+
+public class Transaction {
+    private final Trader trader;
+    private final int year;
+    private final int value;
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "trader=" + trader +
+                ", year=" + year +
+                ", value=" + value +
+                '}';
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Transaction(Trader trader, int year, int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+}
