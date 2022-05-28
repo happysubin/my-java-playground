f05cb9f978cf08829ca5d6d7f2835ca487d16b29
happysubin <asb0711@gmail.com>
2022-05-28 12:06:25 +0900

    실전 자바 소프트웨어 개발 2-8까지

---
+++ src/main/java/happysubin/javapractice/book/realworld/SecondChapter/step4/MainApplication.java
@@ -0,0 +1,13 @@
+package happysubin.javapractice.book.realworld.SecondChapter.step4;
+
+import java.io.IOException;
+
+public class MainApplication {
+    public static void main(String[] args) throws IOException {
+        final BankStatementAnalyzer  bankStatementAnalyzer= new BankStatementAnalyzer();
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
+
+        bankStatementAnalyzer.analyze("/Users/ansubin/Desktop/source/bank.csv",bankStatementParser);
+
+    }
+}
