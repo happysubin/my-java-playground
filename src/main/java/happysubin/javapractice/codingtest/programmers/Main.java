0ee4527af1f65dee7fa339140ace590ae0bf3b02
happysubin <asb0711@gmail.com>
2022-06-05 16:36:36 +0900

    약수의 개수와 덧셈

--- src/main/java/happysubin/javapractice/codingtest/programmers/Main.java
+++ src/main/java/happysubin/javapractice/codingtest/programmers/Main.java
@@ -3,6 +3,7 @@ package happysubin.javapractice.codingtest.programmers;
 import java.lang.reflect.Array;
 import java.util.*;
 
+/*
 public class Main {
 
     static public int[] solution(int[] arr) {
@@ -34,6 +35,35 @@ public class Main {
         int[] arr = {4,3,2,1};
 
         solution(arr);
+
+    }
+}
+
+
+ */
+
+public class Main {
+
+    static public int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i<=right; i++){
+            int count = 0;
+            for(int j = 1; j <= i; j++){
+                if(i%j == 0){
+                    count++;
+                }
+            }
+            if(count%2 == 0) answer += i;
+            else answer -= i;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+
+        int solution = solution(13, 17);
+        System.out.println("solution = " + solution);
 
     }
 }
