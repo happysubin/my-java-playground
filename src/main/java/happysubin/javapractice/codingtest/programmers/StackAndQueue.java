74a63499d858e7f20328693823a2a64e88c97aef
happysubin <asb0711@gmail.com>
2022-06-14 22:13:50 +0900

    스택과 큐 프린터

--- src/main/java/happysubin/javapractice/codingtest/programmers/StackAndQueue.java
+++ src/main/java/happysubin/javapractice/codingtest/programmers/StackAndQueue.java
@@ -3,6 +3,9 @@ package happysubin.javapractice.codingtest.programmers;
 
 import java.util.*;
 
+/*
+//기능 개발
+
 public class StackAndQueue {
     class Process{
         int progressRate;
@@ -60,6 +63,67 @@ public class StackAndQueue {
 
         main.solution(process1, speed1);
 
+
+
+    }
+
+}
+
+
+ */
+
+public class StackAndQueue {
+    class Element{
+        int rank;
+        boolean target;
+
+        public Element(int rank, boolean target) {
+            this.rank = rank;
+            this.target = target;
+        }
+    }
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        ArrayList<Element> list = new ArrayList<>();
+
+        for(int i = 0 ; i < priorities.length; i++){
+            if(i == location) list.add(new Element(priorities[i], true));
+            else list.add(new Element(priorities[i], false));
+        }
+
+        while(list.size() > 0){
+            int count = 0;
+            for (int i = 0 ; i < list.size(); i++) {
+                if(list.get(i).rank <= list.get(0).rank){
+                    count++;
+                }
+                else break;
+            }
+            if(count == list.size()){
+                Element remove = list.remove(0);
+                answer ++;
+                if(remove.target == true){
+                    break;
+                }
+            }
+            else{
+                Element remove = list.remove(0);
+                list.add(remove);
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] priorities= {2, 1, 3, 2};
+        int[] priorities1= {1, 1, 9, 1, 1, 1};
+        int num = 2;
+        StackAndQueue main = new StackAndQueue();
+
+        System.out.println(main.solution(priorities1,0));
 
 
     }
