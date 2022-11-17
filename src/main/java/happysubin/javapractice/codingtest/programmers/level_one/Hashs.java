6c38644d264def3736a40f258edced7579c57d63
happysubin <asb0711@gmail.com>
2022-11-17 20:47:35 +0900

    Feat: 프로그래머스 level 1 해시 폰켓몬

---
+++ src/main/java/happysubin/javapractice/codingtest/programmers/level_one/Hashs.java
@@ -0,0 +1,31 @@
+package happysubin.javapractice.codingtest.programmers.level_one;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Hashs {
+
+    public int solution(int[] nums) {
+        Set set = new HashSet();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        int num = nums.length / 2;
+
+
+        if(set.size() > num){
+            return num;
+        }
+        else{
+            return set.size();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,1,2,3};
+        Hashs hashs = new Hashs();
+        hashs.solution(arr);
+    }
+}
