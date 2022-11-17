7aeb1746d0261625df089517aab72dbec2971812
happysubin <asb0711@gmail.com>
2022-11-17 21:25:59 +0900

    Feat: 프로그래머스 level 1 연습 문제 2016년

--- src/main/java/happysubin/javapractice/codingtest/programmers/level_one/Practice.java
+++ src/main/java/happysubin/javapractice/codingtest/programmers/level_one/Practice.java
@@ -1,21 +1,49 @@
 package happysubin.javapractice.codingtest.programmers.level_one;
 
-public class Practice {
+/**
+ * 가운데 글자 구하기
+ * public class Practice {
+ *
+ *
+ *     public String solution(String s) {
+ *         String answer = "";
+ *
+ *         if(s.length() % 2 != 0){
+ *             return s.substring(s.length() / 2, s.length() / 2 + 1);
+ *         }
+ *
+ *         return s.substring(s.length() / 2 - 1, s.length() / 2 + 1 );
+ *     }
+ *
+ *     public static void main(String[] args) {
+ *         String s = "abcde";
+ *         Practice practice = new Practice();
+ *         String solution = practice.solution(s);
+ *         System.out.println("solution = " + solution);
+ *     }
+ * }
+ */
 
-    public String solution(String s) {
-        String answer = "";
+//2016년
 
-        if(s.length() % 2 != 0){
-            return s.substring(s.length() / 2, s.length() / 2 + 1);
+public class Practice{
+
+    public String solution(int a, int b) {
+        int month = a - 1;
+        int date = b;
+        int[] months = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+
+        for (int i = 0; i < month; i++) {
+            date += months[i];
         }
 
-        return s.substring(s.length() / 2 - 1, s.length() / 2 + 1 );
+        return day[date  % 7];
     }
 
     public static void main(String[] args) {
-        String s = "abcde";
         Practice practice = new Practice();
-        String solution = practice.solution(s);
+        String solution = practice.solution(1, 14);
         System.out.println("solution = " + solution);
     }
 }
