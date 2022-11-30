ce52510d5509d518846e32a06e6aaaaee3e0624c
happysubin <asb0711@gmail.com>
2022-11-30 16:07:51 +0900

    Feat: 문자열 최소 거리 구하기까지

--- src/main/java/happysubin/javapractice/codingtest/inflearn/StringChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/StringChapter.java
@@ -1,7 +1,8 @@
 package happysubin.javapractice.codingtest.inflearn;
 
 import java.io.*;
-
+import java.util.HashMap;
+import java.util.*;
 
 
 // 문자 넣기
@@ -103,37 +104,179 @@ import java.io.*;
 
 // 특정 문자 뒤집기
 
+/**
+ * public class StringChapter{
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         char[] chars = br.readLine().toCharArray();
+ *         int rt = chars.length - 1;
+ *         int lt = 0;
+ *         while(lt < rt){
+ *             char c = chars[lt];
+ *             char s = chars[rt];
+ *             if(Character.isAlphabetic(c) & Character.isAlphabetic(s)){
+ *                 char temp = chars[lt];
+ *                 chars[lt] = chars[rt];
+ *                 chars[rt] = temp;
+ *                 rt --;
+ *                 lt ++;
+ *             }
+ *
+ *             else if (!Character.isAlphabetic(c)){
+ *                 lt ++;
+ *             }
+ *
+ *             else if (!Character.isAlphabetic(s)){
+ *                 rt --;
+ *             }
+ *         }
+ *
+ *         String s = String.valueOf(chars); //이렇게 하면 문자 배열이 문자열로 된다.
+ *         bw.write(s);
+ *         bw.flush();
+ *     }
+ * }
+ */
+
+//중복 문자 제거
+
+/**
+ * public class StringChapter{
+ *     public static void main(String[] args) throws IOException {
+ *         Set<String> hashSet = new LinkedHashSet();
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         String[] arr = br.readLine().split("");
+ *
+ *         for (String s : arr) {
+ *             hashSet.add(s);
+ *         }
+ *
+ *         StringBuffer sf = new StringBuffer();
+ *         for (String s : hashSet) {
+ *            sf.append(s);
+ *         }
+ *
+ *         bw.write(sf.toString());
+ *         bw.flush();
+ *     }
+ * }
+ */
+
+//회문 문자열 검사하기
+
+/**
+ * public class StringChapter{
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         String s = br.readLine().toLowerCase();
+ *         StringBuffer sf = new StringBuffer();
+ *
+ *
+ *         String r = sf.append(s).reverse().toString();
+ *
+ *         if(r.equals(s)){
+ *             bw.write("YES");
+ *         }
+ *         else {
+ *             bw.write("NO");
+ *         }
+ *
+ *         bw.flush();
+ *     }
+ * }
+ */
+
+// 유효한 팰린드롬
+
+/**
+ * public class StringChapter {
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         String s = br.readLine().replaceAll("[^a-zA-Z]","").toLowerCase(Locale.ROOT);
+ *         StringBuffer sf = new StringBuffer();
+ *         String reverse = sf.append(s).reverse().toString();
+ *
+ *         if(s.equals(reverse)){
+ *             bw.write("YES");
+ *         }
+ *         else{
+ *             bw.write("NO");
+ *         }
+ *
+ *         bw.flush();
+ *     }
+ * }
+ */
+
+//숫자만 추출
+
+/**
+ * public class StringChapter {
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         char[] chars = br.readLine().toCharArray();
+ *         StringBuffer sf = new StringBuffer();
+ *         for (int i = 0; i < chars.length; i++) {
+ *             if(Character.isDigit(chars[i])){
+ *                 sf.append(chars[i]);
+ *             }
+ *         }
+ *
+ *         bw.write(Integer.parseInt(sf.toString()) + "\n");
+ *         bw.flush();
+ *
+ *     }
+ * }
+ */
 
 public class StringChapter{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] chars = br.readLine().toCharArray();
-        int rt = chars.length - 1;
-        int lt = 0;
-        while(lt < rt){
-            char c = chars[lt];
-            char s = chars[rt];
-            if(Character.isAlphabetic(c) & Character.isAlphabetic(s)){
-                char temp = chars[lt];
-                chars[lt] = chars[rt];
-                chars[rt] = temp;
-                rt --;
-                lt ++;
-            }
+        String[] s = br.readLine().split(" ");
+        String[] arr = s[0].split("");
+        String word = s[1];
 
-            else if (!Character.isAlphabetic(c)){
-                lt ++;
-            }
+        int d = 999;
+        int[] scores = new int[arr.length];
 
-            else if (!Character.isAlphabetic(s)){
-                rt --;
+        //앞에서 부터 측정함.
+        for (int i = 0; i < arr.length; i++) {
+            if(!arr[i].equals(word)){
+                scores[i] = d;
+                d++;
+            }
+            else if(arr[i].equals(word)){
+                d = 0;
+                scores[i] = 0;
+                d++;
             }
         }
 
-        String s = String.valueOf(chars); //이렇게 하면 문자 배열이 문자열로 된다.
-        bw.write(s);
+        //이제 뒤에서부터도 측정해야함.
+        d = 1000;
+        for (int i = arr.length - 1; i >= 0 ; i--) {
+            if(!arr[i].equals(word)){
+                if(scores[i] > d){
+                    scores[i] = d;
+                }
+                d++;
+            }
+            else if(arr[i].equals(word)){
+                d = 0;
+                scores[i] = 0;
+                d++;
+            }
+        }
+
+        for (int score : scores) {
+            bw.write(score + " ");
+        }
         bw.flush();
     }
 }
-
