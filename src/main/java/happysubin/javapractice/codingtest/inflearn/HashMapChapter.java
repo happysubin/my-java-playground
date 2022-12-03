03ab753fa788ecea4389344b2421d884f73959b7
happysubin <asb0711@gmail.com>
2022-12-03 22:28:14 +0900

    Feat: 아나그램

--- src/main/java/happysubin/javapractice/codingtest/inflearn/HashMapChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/HashMapChapter.java
@@ -5,28 +5,62 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
+//학급 회장
+
+/**
+ * public class HashMapChapter {
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         int num = Integer.parseInt(br.readLine());
+ *         String[] arr = br.readLine().split("");
+ *         Map<String, Integer> map = new HashMap<>();
+ *
+ *
+ *         for (String s : arr) {
+ *             map.put(s, map.getOrDefault(s, 0) + 1);
+ *         }
+ *
+ *
+ *         int max = 0 ;
+ *         String answer = "";
+ *         for (String s : map.keySet()) {
+ *             if(max <map.get(s)){
+ *                 answer = s;
+ *                 max = map.get(s);
+ *             }
+ *         }
+ *
+ *         bw.write(answer);
+ *         bw.flush();
+ *     }
+ * }
+ */
+
 public class HashMapChapter {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split("");
-        Map<String, Integer> map = new HashMap<>();
 
+        String[] arr1 = br.readLine().split("");
+        Map<String, Integer> map1 = new HashMap<>();
 
-        for (String s : arr) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+        String[] arr2 = br.readLine().split("");
+        Map<String, Integer> map2 = new HashMap<>();
+
+        for (String s : arr1) {
+            map1.put(s, map1.getOrDefault(s, 0) + 1);
         }
 
-
-        int max = 0 ;
-        String answer = "";
-        for (String s : map.keySet()) {
-            if(max <map.get(s)){
-                answer = s;
-                max = map.get(s);
-            }
+        for (String s : arr2) {
+            map2.put(s, map2.getOrDefault(s, 0) + 1);
         }
+
+        String answer = "YES";
+        for (String s : map2.keySet()) {
+            if(map2.get(s) != map1.get(s)) answer = "NO";
+        }
+
 
         bw.write(answer);
         bw.flush();
