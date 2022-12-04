fa7fc45f6f9069b034645ca23b93a1b1bfc93604
happysubin <asb0711@gmail.com>
2022-12-05 07:57:09 +0900

    Feat: 해쉬 단원 종료. K번째 큰 수

--- src/main/java/happysubin/javapractice/codingtest/inflearn/HashMapChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/HashMapChapter.java
@@ -114,41 +114,88 @@ import java.util.*;
  * }
  */
 
+//모든 아나그램 찾기
+
+/**
+ * public class HashMapChapter {
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *
+ *         String[] str = br.readLine().split(" ");
+ *         List<Integer> answer = new ArrayList<>();
+ *         int len = Integer.parseInt(str[0]);
+ *         int window = Integer.parseInt(str[1]);
+ *         String[] arr = br.readLine().split(" ");
+ *         Map<String, Integer> map = new HashMap<>();
+ *
+ *
+ *         for (int i = 0; i < window; i++) {
+ *             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+ *         }
+ *
+ *         int lt = 0;
+ *         answer.add(map.keySet().size());
+ *         for (int i = window; i < arr.length; i++) {
+ *             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+ *             if(map.get(arr[lt]) <= 1){
+ *                 map.remove(arr[lt]);
+ *             }
+ *             else{
+ *                 map.put(arr[lt], map.get(arr[lt]) -1 );
+ *             }
+ *             lt++;
+ *             //set.remove(arr[lt++]);
+ *             answer.add(map.keySet().size());
+ *         }
+ *
+ *         for (Integer integer : answer) {
+ *             bw.write(integer + " ");
+ *         }
+ *         bw.flush();
+ *     }
+ * }
+ */
+
+// K번째 큰 수
+
 public class HashMapChapter {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String[] split = br.readLine().split(" ");
+        int len = Integer.parseInt(split[0]);
+        int k = Integer.parseInt(split[1]);
+
         String[] str = br.readLine().split(" ");
-        List<Integer> answer = new ArrayList<>();
-        int len = Integer.parseInt(str[0]);
-        int window = Integer.parseInt(str[1]);
-        String[] arr = br.readLine().split(" ");
-        Map<String, Integer> map = new HashMap<>();
-
-
-        for (int i = 0; i < window; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new TreeSet<>(Collections.reverseOrder());
+        for (String s : str) {
+            list.add(Integer.parseInt(s));
         }
 
-        int lt = 0;
-        answer.add(map.keySet().size());
-        for (int i = window; i < arr.length; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-            if(map.get(arr[lt]) <= 1){
-                map.remove(arr[lt]);
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                for (int l = j + 1; l < list.size(); l++) {
+                    set.add(list.get(i) + list.get(j) + list.get(l));
+                }
             }
-            else{
-                map.put(arr[lt], map.get(arr[lt]) -1 );
-            }
-            lt++;
-            //set.remove(arr[lt++]);
-            answer.add(map.keySet().size());
         }
 
-        for (Integer integer : answer) {
-            bw.write(integer + " ");
+        int cnt = 0;
+        int answer = -1;
+        for (Integer integer : set) {
+            cnt ++;
+            if(cnt == k){
+                answer = integer;
+            }
         }
+
+        bw.write(answer + "\n");
+
+
+
         bw.flush();
     }
 }
