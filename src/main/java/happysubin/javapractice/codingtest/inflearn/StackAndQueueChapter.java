e0d8f2330480d9877fb7d6db7708b4ca80d883a0
happysubin <asb0711@gmail.com>
2022-12-07 23:44:58 +0900

    Feat: 후위 연산식

--- src/main/java/happysubin/javapractice/codingtest/inflearn/StackAndQueueChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/StackAndQueueChapter.java
@@ -39,40 +39,81 @@ import java.util.Stack;
  * }
  */
 
+//괄호 문자 제거
+
+/**
+ * public class StackAndQueueChapter {
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *
+ *         String[] arr = br.readLine().split("");
+ *         Stack<String > stack = new Stack();
+ *
+ *         List<String> answer = new ArrayList<>();
+ *
+ *         for (int i = 0; i < arr.length; i++) {
+ *
+ *             if(arr[i].equals("(")){
+ *                 stack.push(arr[i]);
+ *             }
+ *
+ *             else if(arr[i].equals(")")){
+ *                 while(!stack.peek().equals("(")){
+ *                     stack.pop();
+ *                 }
+ *                 stack.pop();
+ *             }
+ *
+ *             else if(stack.size() == 0){
+ *                 answer.add(arr[i]);
+ *             }
+ *             else {
+ *                 stack.push(arr[i]);
+ *             }
+ *         }
+ *
+ *         for (String s : answer) {
+ *             bw.write(s);
+ *         }
+ *         bw.flush();
+ *     }
+ * }
+ */
 public class StackAndQueueChapter {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         String[] arr = br.readLine().split("");
-        Stack<String > stack = new Stack();
-
-        List<String> answer = new ArrayList<>();
-
+        Stack<Integer> stack = new Stack();
+        int answer = 0;
         for (int i = 0; i < arr.length; i++) {
 
-            if(arr[i].equals("(")){
-                stack.push(arr[i]);
+            if(Character.isDigit(arr[i].charAt(0))){
+                stack.add(Integer.parseInt(arr[i]));
             }
-
-            else if(arr[i].equals(")")){
-                while(!stack.peek().equals("(")){
-                    stack.pop();
-                }
-                stack.pop();
+            else if(arr[i].equals("+")){
+                Integer num1 = stack.pop();
+                Integer num2 = stack.pop();
+                stack.push(num1 + num2);
             }
-
-            else if(stack.size() == 0){
-                answer.add(arr[i]);
+            else if(arr[i].equals("*")){
+                Integer num1 = stack.pop();
+                Integer num2 = stack.pop();
+                stack.push(num1 * num2);
             }
-            else {
-                stack.push(arr[i]);
+            else if(arr[i].equals("-")){
+                Integer num1 = stack.pop();
+                Integer num2 = stack.pop();
+                stack.push(num2 - num1);
+            }
+            else if(arr[i].equals("/")){
+                Integer num1 = stack.pop();
+                Integer num2 = stack.pop();
+                stack.push(num2 / num1);
             }
         }
-
-        for (String s : answer) {
-            bw.write(s);
-        }
+        bw.write(stack.pop() + "\n");
         bw.flush();
     }
 }
