51aa5ba6fb93c33aedda2c808ac603fcd6614483
happysubin <asb0711@gmail.com>
2022-12-10 18:11:02 +0900

    Feat: 응급

--- src/main/java/happysubin/javapractice/codingtest/inflearn/StackAndQueueChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/StackAndQueueChapter.java
@@ -190,32 +190,105 @@ import java.util.*;
  * }
  */
 
+// 교육과정 설계
+
+/**
+ * public class StackAndQueueChapter {
+ *     public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         String[] eProcess = br.readLine().split("");
+ *         String[] arr = br.readLine().split("");
+ *         String answer = "YES";
+ *
+ *         Queue<String> queue = new LinkedList<>();
+ *
+ *         for (String process : eProcess) {
+ *             queue.add(process);
+ *         }
+ *
+ *         for (int i = 0; i < arr.length; i++) {
+ *             if(arr[i].equals(queue.peek())){
+ *                 queue.poll();
+ *             }
+ *         }
+ *
+ *         if(queue.size() == 0){
+ *             bw.write("YES");
+ *         }
+ *         else{
+ *             bw.write("NO");
+ *         }
+ *         bw.flush();
+ *     }
+ * }
+ */
+
 public class StackAndQueueChapter {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] eProcess = br.readLine().split("");
-        String[] arr = br.readLine().split("");
-        String answer = "YES";
+        String[] arr = br.readLine().split(" ");
+        
+        int len = Integer.parseInt(arr[0]);
+        int idx = Integer.parseInt(arr[1]);
 
-        Queue<String> queue = new LinkedList<>();
+        String[] temp = br.readLine().split(" ");
+        Queue<Rank> queue = new LinkedList<>();
 
-        for (String process : eProcess) {
-            queue.add(process);
+        for (int i = 0; i < len; i++) {
+            queue.add(new Rank(i, Integer.parseInt(temp[i])));
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i].equals(queue.peek())){
-                queue.poll();
+        int cnt = 0; //순서
+        boolean flag = false;
+
+        while(queue.size()  > 0 ){
+            Rank ele = queue.poll();
+            for (int i = 0; i < queue.size(); i++) {
+                Rank v = queue.poll();
+                queue.add(v);
+                if(ele.score < v.score){
+                    flag = true;
+                }
+
             }
+
+            if(flag == true){
+                queue.add(ele);
+            }
+            else{
+                cnt++;
+                if(ele.idx == idx){
+                    bw.write(cnt + "");
+                    break;
+                }
+            }
+
+            flag = false;
+
         }
 
-        if(queue.size() == 0){
-            bw.write("YES");
-        }
-        else{
-            bw.write("NO");
-        }
         bw.flush();
     }
 }
+
+class Rank{
+    int idx;
+    int score;
+
+    public Rank(int idx, int score) {
+        this.idx = idx;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Rank{" +
+                "idx=" + idx +
+                ", score=" + score +
+                '}';
+    }
+}
+
