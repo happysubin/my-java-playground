77bc851e1c476c64f24051291351dc396858b5bb
happysubin <asb0711@gmail.com>
2022-12-12 00:17:24 +0900

    Feat: 이분 검색

--- src/main/java/happysubin/javapractice/codingtest/inflearn/SortAndSearchChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/SortAndSearchChapter.java
@@ -224,48 +224,93 @@ import java.util.*;
  * }
  */
 
+//좌표 정렬
+
+
+/**
+ * public class SortAndSearchChapter {
+ *
+ *     public void solution() throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         int len = Integer.parseInt(br.readLine());
+ *         List<Point> list = new ArrayList<>();
+ *
+ *         for (int i = 0; i < len; i++) {
+ *             String[] str = br.readLine().split(" ");
+ *             list.add(new Point(Integer.parseInt(str[0]), Integer.parseInt(str[1])));
+ *         }
+ *
+ *         Collections.sort(list, new Comparator<Point>() {
+ *             @Override
+ *             public int compare(Point o1, Point o2) {
+ *                 if(o1.x ==  o2.x) return o1.y - o2.y;
+ *                 else return o1.x - o2.x;
+ *             }
+ *         });
+ *
+ *         for (Point point : list) {
+ *             bw.write(point.x + " " + point.y + "\n");
+ *         }
+ *         bw.flush();
+ *     }
+ *
+ *     public static void main(String[] args) throws IOException {
+ *         SortAndSearchChapter main = new SortAndSearchChapter();
+ *         main.solution();
+ *     }
+ *
+ *
+ *     class Point{
+ *         int x;
+ *         int y;
+ *
+ *         public Point(int x, int y) {
+ *             this.x = x;
+ *             this.y = y;
+ *         }
+ *     }
+ * }
+ */
 
 public class SortAndSearchChapter {
 
-    public void solution() throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int len = Integer.parseInt(br.readLine());
-        List<Point> list = new ArrayList<>();
+        String[] str = br.readLine().split(" ");
+        int len = Integer.parseInt(str[0]);
+        int target = Integer.parseInt(str[1]);
 
-        for (int i = 0; i < len; i++) {
-            String[] str = br.readLine().split(" ");
-            list.add(new Point(Integer.parseInt(str[0]), Integer.parseInt(str[1])));
+        String[] temp = br.readLine().split(" ");
+        int[] arr = new int[temp.length];
+
+        for (int i = 0; i < temp.length; i++) {
+            arr[i] = Integer.parseInt(temp[i]);
         }
 
-        Collections.sort(list, new Comparator<Point>() {
-            @Override
-            public int compare(Point o1, Point o2) {
-                if(o1.x ==  o2.x) return o1.y - o2.y;
-                else return o1.x - o2.x;
+        Arrays.sort(arr);
+
+        int lt = 0, rt = len - 1;
+        int answer = 0;
+        while(lt <= rt){
+            int mid = (lt + rt ) / 2;
+            if(arr[mid] == target){
+                answer = mid + 1;
+                break;
             }
-        });
 
-        for (Point point : list) {
-            bw.write(point.x + " " + point.y + "\n");
+            else if(arr[mid] > target){
+                rt = mid - 1;
+            }
+
+            else{
+                lt = mid + 1;
+            }
         }
+
+        bw.write(answer + " ");
         bw.flush();
-    }
-
-    public static void main(String[] args) throws IOException {
-        SortAndSearchChapter main = new SortAndSearchChapter();
-        main.solution();
-    }
-
-
-    class Point{
-        int x;
-        int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
 
