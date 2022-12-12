bdc658da200cd918f807a526fd2102a2ccfa7e47
happysubin <asb0711@gmail.com>
2022-12-12 16:04:21 +0900

    Feat: 바이트 코드 조작하기. 컴파일된 .class 파일 리턴 값이 변경된 것을 확인함

--- src/main/java/happysubin/javapractice/the_java/how_to_manipulate_code/bytecode/Moim.java
+++ src/main/java/happysubin/javapractice/the_java/how_to_manipulate_code/bytecode/Moim.java
@@ -20,7 +20,11 @@ public class Moim {
 
 /**
  * https://docs.gradle.org/current/userguide/jacoco_plugin.html
- * jacoco 적용
- *
+ * jacoco 적용 *
  * https://techblog.woowahan.com/2661/
+ */
+
+
+/**
+ * ByteBuddy 바이트 코드 조작 라이브러리. 이걸 선장님이 추천해주심.
  */
---
+++ src/main/java/happysubin/javapractice/the_java/how_to_manipulate_code/bytecode/Moja.java
@@ -0,0 +1,8 @@
+package happysubin.javapractice.the_java.how_to_manipulate_code.bytecode;
+
+public class Moja {
+
+    public String pullOut(){
+        return "";
+    }
+}
