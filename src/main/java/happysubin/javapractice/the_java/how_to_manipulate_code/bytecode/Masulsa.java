fe6e95ed9e7b451e2b10d91e6b3877e3b72c1991
happysubin <asb0711@gmail.com>
2022-12-12 16:42:24 +0900

    Feat: 순수 자바 바이트 코드 조작 파트 마무리

--- src/main/java/happysubin/javapractice/the_java/how_to_manipulate_code/bytecode/Masulsa.java
+++ src/main/java/happysubin/javapractice/the_java/how_to_manipulate_code/bytecode/Masulsa.java
@@ -10,9 +10,15 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args) throws IOException {
-        new ByteBuddy().redefine(Moja.class)
-                .method(named("pullOut")).intercept(FixedValue.value("Rabbit!!!!!"))
-                .make().saveIn(new File("/Users/ansubin/Desktop/java-practice/build/classes/java/main/happysubin/javapractice/the_java/how_to_manipulate_code/bytecode/byte/"));
+
+//        new ByteBuddy().redefine(Moja.class)
+//                .method(named("pullOut")).intercept(FixedValue.value("Rabbit!!!!!"))
+//                .make().saveIn(new File("/Users/ansubin/Desktop/java-practice/build/classes/java/main/happysubin/javapractice/the_java/how_to_manipulate_code/bytecode/byte/"));
+
+
+
         System.out.println(new Moja().pullOut());
+
+
     }
 }
