b477c25e03009245c7394e5259012b14fe6f7794
happysubin <asb0711@gmail.com>
2022-12-14 02:22:52 +0900

    Feat: 부분집합 구하기

--- src/main/java/happysubin/javapractice/codingtest/inflearn/DFSAndBFSChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/DFSAndBFSChapter.java
@@ -83,38 +83,67 @@ package happysubin.javapractice.codingtest.inflearn;
 
 //이진트리 순회까지
 
-public class DFSAndBFSChapter {
+/**
+ * public class DFSAndBFSChapter {
+ *
+ *     Node root;
+ *     public void DFS(Node root){
+ *         if(root==null)
+ *             return;
+ *         else{
+ *             DFS(root.lt);
+ *             System.out.print(root.data+" ");
+ *             DFS(root.rt);
+ *         }
+ *     }
+ *
+ *     public static void main(String args[]) {
+ *         DFSAndBFSChapter tree=new DFSAndBFSChapter();
+ *         tree.root=new Node(1);
+ *         tree.root.lt=new Node(2);
+ *         tree.root.rt=new Node(3);
+ *         tree.root.lt.lt=new Node(4);
+ *         tree.root.lt.rt=new Node(5);
+ *         tree.root.rt.lt=new Node(6);
+ *         tree.root.rt.rt=new Node(7);
+ *         tree.DFS(tree.root);
+ *     }
+ * }
+ *
+ *
+ * class Node{
+ *     int data;
+ *     Node lt, rt;
+ *     public Node(int val) {
+ *         data=val;
+ *         lt=rt=null;
+ *     }
+ * }
+ */
 
-    Node root;
-    public void DFS(Node root){
-        if(root==null)
-            return;
+class DFSAndBFSChapter {
+    static int n;
+    static int[] ch;
+    public void DFS(int L){
+        if(L==n+1){
+            String tmp="";
+            for(int i=1; i<=n; i++){
+                if(ch[i]==1) tmp+=(i+" ");
+            }
+            if(tmp.length()>0) System.out.println(tmp);
+        }
         else{
-            DFS(root.lt);
-            System.out.print(root.data+" ");
-            DFS(root.rt);
+            ch[L]=1; //사용한다.
+            DFS(L+1);
+            ch[L]=0; //사용하지 않는다.
+            DFS(L+1);
         }
     }
 
-    public static void main(String args[]) {
-        DFSAndBFSChapter tree=new DFSAndBFSChapter();
-        tree.root=new Node(1);
-        tree.root.lt=new Node(2);
-        tree.root.rt=new Node(3);
-        tree.root.lt.lt=new Node(4);
-        tree.root.lt.rt=new Node(5);
-        tree.root.rt.lt=new Node(6);
-        tree.root.rt.rt=new Node(7);
-        tree.DFS(tree.root);
-    }
-}
-
-
-class Node{
-    int data;
-    Node lt, rt;
-    public Node(int val) {
-        data=val;
-        lt=rt=null;
+    public static void main(String[] args){
+        DFSAndBFSChapter T = new DFSAndBFSChapter();
+        n=3;
+        ch=new int[n+1];
+        T.DFS(1);
     }
 }
