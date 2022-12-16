463f6998df32c643193a1eb89844344a08fb8f77
happysubin <asb0711@gmail.com>
2022-12-16 21:31:33 +0900

    Feat: 말단 노드까지의 가장 짧은 경로(DFS)

--- src/main/java/happysubin/javapractice/codingtest/inflearn/DFSAndBFSChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/DFSAndBFSChapter.java
@@ -255,45 +255,79 @@ import java.util.Queue;
  */
 
 
+// 송아지 찾기
+
+/**
+ * import java.util.*;
+ * public class DFSAndBFSChapter {
+ *     int answer=0;
+ *     int[] dis={1, -1, 5};
+ *     int[] ch;
+ *     Queue<Integer> Q = new LinkedList<>();
+ *     public int BFS(int s, int e){
+ *         ch=new int[10001];
+ *         ch[s]=1;
+ *         Q.offer(s);
+ *         int L=0;
+ *         while(!Q.isEmpty()){
+ *             int len=Q.size();
+ *             for(int i=0; i<len; i++){
+ *                 int x = Q.poll();
+ *                 for(int j=0; j<3; j++){
+ *                     int nx=x+dis[j];
+ *                     if(nx==e){
+ *                         return L+1;
+ *                     }
+ *                     if(nx>=1 && nx<=10000 && ch[nx]==0){
+ *                         ch[nx]=1;
+ *                         Q.offer(nx);
+ *                     }
+ *                 }
+ *             }
+ *             L++;
+ *         }
+ *         return 0;
+ *     }
+ *
+ *     public static void main(String[] args){
+ *         DFSAndBFSChapter T = new DFSAndBFSChapter();
+ *         Scanner kb = new Scanner(System.in);
+ *         int s=kb.nextInt();
+ *         int e=kb.nextInt();
+ *         System.out.println(T.BFS(s, e));
+ *     }
+ * }
+ */
 
 
-import java.util.*;
-public class DFSAndBFSChapter {
-    int answer=0;
-    int[] dis={1, -1, 5};
-    int[] ch;
-    Queue<Integer> Q = new LinkedList<>();
-    public int BFS(int s, int e){
-        ch=new int[10001];
-        ch[s]=1;
-        Q.offer(s);
-        int L=0;
-        while(!Q.isEmpty()){
-            int len=Q.size();
-            for(int i=0; i<len; i++){
-                int x = Q.poll();
-                for(int j=0; j<3; j++){
-                    int nx=x+dis[j];
-                    if(nx==e){
-                        return L+1;
-                    }
-                    if(nx>=1 && nx<=10000 && ch[nx]==0){
-                        ch[nx]=1;
-                        Q.offer(nx);
-                    }
-                }
-            }
-            L++;
-        }
-        return 0;
-    }
-
-    public static void main(String[] args){
-        DFSAndBFSChapter T = new DFSAndBFSChapter();
-        Scanner kb = new Scanner(System.in);
-        int s=kb.nextInt();
-        int e=kb.nextInt();
-        System.out.println(T.BFS(s, e));
+class Node{
+    int data;
+    Node lt, rt;
+    public Node(int val) {
+        data=val;
+        lt=rt=null;
     }
 }
+
+public class DFSAndBFSChapter{
+
+    private Node root;
+
+    public static void main(String args[]) {
+        DFSAndBFSChapter tree=new DFSAndBFSChapter();
+        tree.root=new Node(1);
+        tree.root.lt=new Node(2);
+        tree.root.rt=new Node(3);
+        tree.root.lt.lt=new Node(4);
+        tree.root.lt.rt=new Node(5);
+        System.out.println(tree.DFS(0, tree.root));
+    }
+
+    public int DFS(int L, Node root){
+        if(root.lt==null && root.rt==null) return L;
+        else return Math.min(DFS(L+1, root.lt), DFS(L+1, root.rt));
+    }
+}
+
+
 
