aca9fcbb1c8f5252cb8613914610a58b217547fe
happysubin <asb0711@gmail.com>
2022-12-24 15:42:53 +0900

    Feat: 바둑이 승차

--- src/main/java/happysubin/javapractice/codingtest/inflearn/DFSAndBFSUtilizeChapter.java
+++ src/main/java/happysubin/javapractice/codingtest/inflearn/DFSAndBFSUtilizeChapter.java
@@ -3,43 +3,91 @@ package happysubin.javapractice.codingtest.inflearn;
 import java.io.*;
 import java.util.*;
 
+/** 합이 같은 부분집합
+ * public class DFSAndBFSUtilizeChapter {
+ *
+ *      static List<Integer> list = new ArrayList<>();
+ *      static int[] ch;
+ *      static int answer = 0;
+ *
+ *     public static void main(String[] args) throws IOException {
+ *
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         int num = Integer.parseInt(br.readLine());
+ *         String[] s = br.readLine().split(" ");
+ *
+ *         for (String s1 : s) {
+ *             list.add(Integer.parseInt(s1));
+ *         }
+ *         ch = new int[num];
+ *
+ *         DFSAndBFSUtilizeChapter main = new DFSAndBFSUtilizeChapter();
+ *         main.DFS(-1, ch);
+ *         if(answer >= 1) bw.write("YES");
+ *         else bw.write("NO");
+ *         bw.flush();
+ *     }
+ *
+ *     private void DFS(int L, int[] ch) {
+ *         if(L == ch.length - 1){
+ *             int sum1 = 0, sum2 = 0;
+ *             for (int i = 0; i < ch.length; i++) {
+ *                 if(ch[i] == 1){
+ *                     sum2 += list.get(i);
+ *                 }
+ *                 else{
+ *                     sum1 += list.get(i);
+ *                 }
+ *             }
+ *             if(sum2 == sum1)answer++;
+ *         }
+ *         else{
+ *             L++;
+ *             ch[L] = 1;
+ *             DFS(L, ch);
+ *             ch[L] = 0;
+ *             DFS(L, ch);
+ *         }
+ *     }
+ * }
+ */
+
 public class DFSAndBFSUtilizeChapter {
 
-     static List<Integer> list = new ArrayList<>();
-     static int[] ch;
-     static int answer = 0;
+    static List<Integer> list = new ArrayList<>();
+    static int limit = 0;
+    static int max = 0;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        String[] s = br.readLine().split(" ");
+        Scanner sc = new Scanner(System.in);
+        limit = sc.nextInt();
+        int num = sc.nextInt();
 
-        for (String s1 : s) {
-            list.add(Integer.parseInt(s1));
+        for (int i = 0; i < num; i++) {
+            list.add(sc.nextInt());
         }
-        ch = new int[num];
+
+        int[] ch = new int[num];
 
         DFSAndBFSUtilizeChapter main = new DFSAndBFSUtilizeChapter();
         main.DFS(-1, ch);
-        if(answer >= 1) bw.write("YES");
-        else bw.write("NO");
-        bw.flush();
+        System.out.println(max);
     }
 
     private void DFS(int L, int[] ch) {
+        int sum = 0;
         if(L == ch.length - 1){
-            int sum1 = 0, sum2 = 0;
+
             for (int i = 0; i < ch.length; i++) {
-                if(ch[i] == 1){
-                    sum2 += list.get(i);
-                }
-                else{
-                    sum1 += list.get(i);
-                }
+                if(ch[i] == 1) sum += list.get(i);
             }
-            if(sum2 == sum1)answer++;
+
+            if(sum < limit){
+                max = Math.max(max,sum);
+            }
+
         }
         else{
             L++;
