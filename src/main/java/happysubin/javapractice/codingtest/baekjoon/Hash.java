94e89681b2bd8a7bf7f95840fa5a1bca62b478a2
happysubin <asb0711@gmail.com>
2023-05-01 17:03:41 +0900

    포켓몬 마스터 이다솜

--- src/main/java/happysubin/javapractice/codingtest/baekjoon/Hash.java
+++ src/main/java/happysubin/javapractice/codingtest/baekjoon/Hash.java
@@ -33,3 +33,37 @@ public class Hash{
         bw.flush();
     }
 }
+
+
+//포켓몬 마스터 이다솜.
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] str = br.readLine().split(" ");
+        int total = Integer.parseInt(str[0]); //전체 포켓몬 수
+        int problem = Integer.parseInt(str[1]); //맞춰야하는 문제 개수
+
+        Map<String, String> numMap = new HashMap<>();
+        Map<String, String> stringMap = new HashMap<>();
+
+        //도감 초기화
+        for(int i = 1; i <= total; i++){
+            String pokemon = br.readLine();
+            numMap.put(String.valueOf(i), pokemon);
+            stringMap.put(pokemon, String.valueOf(i));
+        }
+
+        //문제가 들어와
+        for(int i = 0; i < problem; i++){
+            String s = br.readLine();
+            if(numMap.keySet().contains(s)){
+                bw.write(numMap.get(s) + "\n");
+            }
+            else{
+                bw.write(stringMap.get(s) + "\n");
+            }
+        }
+        bw.flush();
+    }
+}
