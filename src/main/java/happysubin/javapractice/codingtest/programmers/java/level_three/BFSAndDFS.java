9661de154fdc381da6c749a86f643c0936190b3d
happysubin <asb0711@gmail.com>
2023-05-03 22:36:19 +0900

    프로그래머스: 단어 변환 BFS

--- src/main/java/happysubin/javapractice/codingtest/programmers/java/level_three/BFSAndDFS.java
+++ src/main/java/happysubin/javapractice/codingtest/programmers/java/level_three/BFSAndDFS.java
@@ -38,55 +38,99 @@ import java.util.*;
 //    }
 //}
 
-//단어 변환
-class BFSAndDFS {
+//단어 변환 DFS -> 올바른 풀이가 아닌듯.
+//class BFSAndDFS {
+//
+//    static boolean[] visited;
+//    static int answer = Integer.MAX_VALUE;
+//
+//    public int ladderLength(String beginWord, String endWord, List<String> wordList){
+//        String[] words = new String[wordList.size()];
+//        for(int i = 0; i < wordList.size(); i++){
+//            words[i] = wordList.get(i);
+//        }
+//
+//        visited = new boolean[words.length];
+//        dfs(beginWord, endWord, words, 0);
+//        if(answer == Integer.MAX_VALUE) return 0;
+//        return answer;
+//    }
+//
+//    private void dfs(String begin, String target, String[] words, int cnt){
+//        //같다면 끝
+//        if(begin.equals(target)){
+//            answer = Math.min(answer, cnt + 1);
+//        }
+//        //더 많은 곳을 탐색하면 의미가 없다.
+//        if(cnt == words.length - 1){
+//            return;
+//        }
+//        for(int j = 0; j < words.length; j++){ //단어를 반복
+//            String s = words[j];
+//            String[] temp1 = s.split("");
+//            String[] temp2 = begin.split("");
+//            int check = 0;
+//            int i = 0;
+//            for(; i < temp1.length; i++){
+//                if(!temp1[i].equals(temp2[i])){
+//                    check++;
+//                }
+//            }
+//            if(check == 1 && visited[j] == false){
+//                visited[j] = true;
+//                dfs(s, target, words, cnt + 1);
+//                visited[j] = false;
+//            }
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        BFSAndDFS bfsAndDFS = new BFSAndDFS();
+//        int i = bfsAndDFS.ladderLength("hit", "cog", List.of("hot", "dot", "dog", "lot", "log"));
+//        System.out.println("i = " + i);
+//    }
+//}
 
-    static boolean[] visited;
-    static int answer = Integer.MAX_VALUE;
+import java.util.*;
+class Solution {
+
+    class Count{
+        String name;
+        int count;
+        public Count(String name, int count){
+            this.name = name;
+            this.count = count;
+        }
+    }
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList){
-        String[] words = new String[wordList.size()];
-        for(int i = 0; i < wordList.size(); i++){
-            words[i] = wordList.get(i);
-        }
+        boolean[] visited = new boolean[wordList.size()];
+        Queue<Count> queue = new LinkedList<>();
+        queue.add(new Count(beginWord, 1));
 
-        visited = new boolean[words.length];
-        dfs(beginWord, endWord, words, 0);
-        if(answer == Integer.MAX_VALUE) return 0;
-        return answer;
-    }
+        while(!queue.isEmpty()){
+            Count count = queue.poll(); //값을 꺼냄
+            if(count.name.equals(endWord)){
+                return count.count;
+            }
+            String[] str = count.name.split(""); //꺼낸 이름을 분리함.
 
-    private void dfs(String begin, String target, String[] words, int cnt){
-        //같다면 끝
-        if(begin.equals(target)){
-            answer = Math.min(answer, cnt + 1);
-        }
-        //더 많은 곳을 탐색하면 의미가 없다.
-        if(cnt == words.length - 1){
-            return;
-        }
-        for(int j = 0; j < words.length; j++){ //단어를 반복
-            String s = words[j];
-            String[] temp1 = s.split("");
-            String[] temp2 = begin.split("");
-            int check = 0;
-            int i = 0;
-            for(; i < temp1.length; i++){
-                if(!temp1[i].equals(temp2[i])){
-                    check++;
+            for(int i = 0; i< wordList.size(); i++){ // 모든 단어를 가져와서 꺼낸 값과 비교해야함
+                String word = wordList.get(i);
+                String[] s = word.split(""); //들어온 단어 분리
+                int cnt = 0;
+                int j = 0;
+                for(; j < str.length; j++){
+                    if(!str[j].equals(s[j])){ //1개만 차이가나는지 비교
+                        cnt++;
+                    }
+                }
+                if(cnt == 1 && visited[i] == false){
+                    visited[i] = true;
+                    queue.add(new Count(word, count.count + 1));
                 }
             }
-            if(check == 1 && visited[j] == false){
-                visited[j] = true;
-                dfs(s, target, words, cnt + 1);
-                visited[j] = false;
-            }
         }
-    }
-
-    public static void main(String[] args) {
-        BFSAndDFS bfsAndDFS = new BFSAndDFS();
-        int i = bfsAndDFS.ladderLength("hit", "cog", List.of("hot", "dot", "dog", "lot", "log"));
-        System.out.println("i = " + i);
+        return 0;
     }
 }
--- src/main/java/happysubin/javapractice/codingtest/programmers/java/level_two/BruteForce.java
+++ src/main/java/happysubin/javapractice/codingtest/programmers/java/level_two/BruteForce.java
@@ -1,50 +1,51 @@
 package happysubin.javapractice.codingtest.programmers.java.level_two;
 
+import java.io.*;
 import java.util.*;
 
 //소수기
-class BruteForce {
-    Set<Integer> set = new HashSet<>();
-    public int solution(String numbers) {
-        //1. 문자열의 문자들을 문자열 배열로 만든다.
-        String[] arr = numbers.split("");
-
-        //2. 방문했는지 체크하는 배열
-        boolean[] visited = new boolean[numbers.length()];
-
-        //4. 숫자를 합친 경우를 탐색해야 한다.
-        for(int i = 0; i < numbers.length(); i++){
-            visited[i] = true;
-            dfs(arr[i], arr, visited, 1);
-            visited[i] = false;
-        }
-        return set.size();
-    }
-
-    private void dfs(String start, String[] arr,boolean[] visited, int cnt){
-
-        int result = Integer.parseInt(start);
-
-        for(int i = 2; i <= result; i++ ){
-            if(result % i ==0){
-                if(i != result){
-                    break;
-                }
-                else{
-                    set.add(result);
-                }
-            }
-        }
-
-        for(int i = 0; i < arr.length; i++){
-            if(visited[i] == false){
-                visited[i] = true;
-                dfs(start + arr[i], arr, visited, cnt + 1);
-                visited[i] = false;
-            }
-        }
-    }
-}
+//class BruteForce {
+//    Set<Integer> set = new HashSet<>();
+//    public int solution(String numbers) {
+//        //1. 문자열의 문자들을 문자열 배열로 만든다.
+//        String[] arr = numbers.split("");
+//
+//        //2. 방문했는지 체크하는 배열
+//        boolean[] visited = new boolean[numbers.length()];
+//
+//        //4. 숫자를 합친 경우를 탐색해야 한다.
+//        for(int i = 0; i < numbers.length(); i++){
+//            visited[i] = true;
+//            dfs(arr[i], arr, visited, 1);
+//            visited[i] = false;
+//        }
+//        return set.size();
+//    }
+//
+//    private void dfs(String start, String[] arr,boolean[] visited, int cnt){
+//
+//        int result = Integer.parseInt(start);
+//
+//        for(int i = 2; i <= result; i++ ){
+//            if(result % i ==0){
+//                if(i != result){
+//                    break;
+//                }
+//                else{
+//                    set.add(result);
+//                }
+//            }
+//        }
+//
+//        for(int i = 0; i < arr.length; i++){
+//            if(visited[i] == false){
+//                visited[i] = true;
+//                dfs(start + arr[i], arr, visited, cnt + 1);
+//                visited[i] = false;
+//            }
+//        }
+//    }
+//}
 
 //카펫
 class BruteForce {
