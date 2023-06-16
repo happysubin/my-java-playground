4588851ac4e289caae1f1ae0efe40f0db6052ccf
happysubin <asb0711@gmail.com>
2023-06-16 12:34:42 +0900

    재고 시스템 세팅

---
+++ src/main/java/happysubin/javapractice/lecture/concurrency/ConCurrencyApplication.java
@@ -0,0 +1,12 @@
+package happysubin.javapractice.lecture.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConCurrencyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConCurrencyApplication.class, args);
+    }
+}
+
