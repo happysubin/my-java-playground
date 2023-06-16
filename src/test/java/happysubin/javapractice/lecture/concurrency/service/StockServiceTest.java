3112baf964e966edb2d4b2db9597e3e5815eb5ac
happysubin <asb0711@gmail.com>
2023-06-16 12:51:25 +0900

    현재 로직 문제점 확인

--- src/test/java/happysubin/javapractice/lecture/concurrency/service/StockServiceTest.java
+++ src/test/java/happysubin/javapractice/lecture/concurrency/service/StockServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -40,4 +44,26 @@ class StockServiceTest {
         Assertions.assertThat(stock.getQuantity()).isEqualTo(99);
     }
 
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await(); //대기
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(0); //기대하는 값인 0이 나오지 않는다. 경쟁 조건이 발생했기 때문!!!
+    }
 }
