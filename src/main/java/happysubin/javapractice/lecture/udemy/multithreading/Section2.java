327a5e3c707d5e48166851ac44449c53ce965d4d
happysubin <asb0711@gmail.com>
2023-11-14 20:41:27 +0900

    udemy 멀티 스레드 강의 시작

---
+++ src/main/java/happysubin/javapractice/lecture/udemy/multithreading/Section2.java
@@ -0,0 +1,28 @@
+package happysubin.javapractice.lecture.udemy.multithreading;
+
+public class Section2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            System.out.println("Hello World!");
+            System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+            System.out.println("Thread.currentThread().getPriority() = " + Thread.currentThread().getPriority());
+            throw new RuntimeException("exception");
+        });
+        thread.setName("awesome-thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(Thread.currentThread().getName());
+        thread.start();
+        System.out.println(Thread.currentThread().getName());
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("t.getName() + e.getMessage() = " + t.getName() + e.getMessage());
+            }
+        });
+
+        Thread.sleep(3000);
+
+    }
+}
