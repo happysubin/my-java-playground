6fd2877c0e6c18f2adff50fab5807daf2fde3a62
happysubin <asb0711@gmail.com>
2023-12-17 01:36:08 +0900

    인프런 자바 동시성 프로그매이 파트 1 - synchronized 메서드 동기화

--- src/main/java/happysubin/javapractice/lecture/inflearn/java_concurreny_part1/chapter06/mornitor/ConditionSynchronizationExample.java
+++ src/main/java/happysubin/javapractice/lecture/inflearn/java_concurreny_part1/chapter06/mornitor/ConditionSynchronizationExample.java
@@ -1,2 +1,52 @@
-package happysubin.javapractice.lecture.inflearn.java_concurreny_part1.chapter06.mornitor;public class ConditionSynchronizationExample {
+package happysubin.javapractice.lecture.inflearn.java_concurreny_part1.chapter06.mornitor;
+
+public class ConditionSynchronizationExample {
+
+    private boolean isAvailable = false;
+
+    public synchronized void produce() {
+        while (isAvailable) {
+            try {
+                wait();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " 생산됨.");
+        isAvailable = true;
+        notify();
+    }
+
+    public synchronized void consume() {
+        while (!isAvailable) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " 소비됨.");
+        isAvailable = false;
+        notify();
+    }
+
+    public static void main(String[] args) {
+        ConditionSynchronizationExample example = new ConditionSynchronizationExample();
+
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                example.produce();
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                example.consume();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+    }
 }
--- src/main/java/happysubin/javapractice/lecture/inflearn/java_concurreny_part1/chapter06/mornitor/MutualExclusionExample.java
+++ src/main/java/happysubin/javapractice/lecture/inflearn/java_concurreny_part1/chapter06/mornitor/MutualExclusionExample.java
@@ -1,2 +1,29 @@
-package happysubin.javapractice.lecture.inflearn.java_concurreny_part1.chapter06.mornitor;public class MutualExclusionExample {
+package happysubin.javapractice.lecture.inflearn.java_concurreny_part1.chapter06.mornitor;
+
+public class MutualExclusionExample {
+    private int counter = 0;
+
+    public synchronized void increment() {
+        counter++;
+        System.out.println("스레드: " + Thread.currentThread().getName() + ", 카운터 값: " + counter);
+    }
+
+    public static void main(String[] args) {
+        MutualExclusionExample example = new MutualExclusionExample();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                example.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                example.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
 }
