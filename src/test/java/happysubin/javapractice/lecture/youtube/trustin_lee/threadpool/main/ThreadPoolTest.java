54a3ecaac21336e7dd67f98d90d20af6eea2dad6
happysubin <asb0711@gmail.com>
2025-04-15 22:38:45 +0900

    이희승님 유튜브: CountLatchDown을 활용한 테스트 코드 작성

--- src/test/java/happysubin/javapractice/lecture/youtube/trustin_lee/threadpool/main/ThreadPoolTest.java
+++ src/test/java/happysubin/javapractice/lecture/youtube/trustin_lee/threadpool/main/ThreadPoolTest.java
@@ -3,6 +3,7 @@ package happysubin.javapractice.lecture.youtube.trustin_lee.threadpool.main;
 import happysubin.javapractice.lecture.inflearn.java_concurreny_part1.chapter12.completablefuture_start_api.RunAsyncExample;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 
 
@@ -10,9 +11,10 @@ class ThreadPoolTest {
 
 
     @Test
-    public void submitTasksAreExecuted() {
+    public void submitTasksAreExecuted() throws InterruptedException {
         final Executor executor = new ThreadPool(2);
-
+        final int numTasks = 100;
+        final CountDownLatch latch = new CountDownLatch(numTasks);
 
         /**
          * 한 스레드 안에서 순서는 맞다
@@ -27,7 +29,7 @@ class ThreadPoolTest {
          * Thread-4 executes a task 9
          * Thread-3 executes a task 7
          */
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numTasks; i++) {
             final int finalI = i;
             executor.execute(() -> {
                 System.out.println(Thread.currentThread().getName() + " executes a task " + finalI);
@@ -37,14 +39,19 @@ class ThreadPoolTest {
                  * Thread-3 executes a task 0
                  * Thread-4 executes a task 1
                  *
-                 * 테스트 메서드가 그냥 종료되버리기 때문
+                 * 테스트 메서드가 그냥 종료되버리기 때문.
+                 *
+                 * 따라서 CountDownLatch을 사용
                  */
                 try {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                latch.countDown();
             });
         }
+
+        latch.await();
     }
 }
